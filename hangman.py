1b01ebbd2cc0edb1247f0e0faae5bf599a91e287
teenageknight <davidgeorge0227@gmail.com>
2018-07-10 20:38:45 -0400

    Changes

--- hangman.py
+++ hangman.py
@@ -22,7 +22,6 @@ from art import *
 #getWordFile()
 #
 #Get a txt file with the words required for the game
-
 def getWordFile():
     try:
         #Check for "wordlist.txt"
@@ -33,7 +32,94 @@ def getWordFile():
         file_name = input('Please specify a text file containing a list of words for the Hangman game to choose from (include the full file path if the file is in a different directory than the Hangman program): ')
         while not(found_file):
             try:
-                with open(file_name, "r": found_file= True)
+                with open(file_name, "r"): found_file= True
             except IOError:
                 file_name = input('\n{0} was not found!\n\nPlease try again, or specify a different file (include the full file path if the file is in a different directory than the Hangman program): '.format(file_name))
     return file_name
+
+# chooseWord()
+# Chooses a word randomly from the list of words taken from the input file
+def chooseWord(file_name):
+    infile = open(file_name, 'r')
+    wordlist = infile.readlines()
+    total_words = len(wordlist)
+    random_num = randint(0, total_words - 1)
+
+    chosen_word = wordlist[random_num].replace('\n', '')
+    word_len = len(chosen_word)
+    return chosen_word, word_len
+
+def drawMan(strike):
+    if strike == 1:
+        print("   _ _")
+        print("  |   |")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("==========")
+
+    elif strike == 2:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("==========")
+
+    elif strike == 3:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print("  |   |")
+        print("      |")
+        print("      |")
+        print("==========")
+
+    elif strike == 4:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print(" /|   |")
+        print("      |")
+        print("      |")
+        print("==========")
+
+    elif strike == 5:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print(" /|\  |")
+        print("      |")
+        print("      |")
+        print("==========")
+
+    elif strike == 6:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print(" /|\  |")
+        print(" /    |")
+        print("      |")
+        print("==========")
+
+    elif strike == 7:
+        print("   _ _")
+        print("  |   |")
+        print("  O   |")
+        print(" /|\  |")
+        print(" / \  |")
+        print("      |")
+        print("==========")
+
+
+# Define Main Game loop
+def main():
+    file = getWordFile()
+    word, length = chooseWord(file)
+    print(word)
+    print(length)
+    print(hangman)
+
+main()
