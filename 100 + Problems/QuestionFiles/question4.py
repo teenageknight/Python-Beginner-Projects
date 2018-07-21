63d02c0d4a525ed7717000d5d2ebe01181bbeed3
teenageknight <davidgeorge0227@gmail.com>
2018-07-20 23:02:02 -0400

    Debuged qestions 3-5

---
+++ 100 + Problems/QuestionFiles/question4.py
@@ -0,0 +1,15 @@
+'''
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+'''
+
+values = input()
+l = values.split(',')
+t = tuple(l)
+
+print(l)
+print(t)
---
+++ 100 + Problems/QuestionFiles/question5.py
@@ -0,0 +1,23 @@
+'''
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+'''
+
+class InputOutString(object):
+    """docstring for InputOutString."""
+
+    def __init__(self):
+        self.s = ''
+
+    def getString(self):
+        self.s = str(input())
+
+    def printString(self):
+        print(self.s.upper())
+        
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
