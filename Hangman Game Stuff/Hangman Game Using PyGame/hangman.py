adabf9ca573572921761c2ac7c8a72089c895c8f
teenageknight <davidgeorge0227@gmail.com>
2018-07-31 12:25:45 -0400

    new input

--- Hangman Game Stuff/Hangman Game Using PyGame/hangman.py
+++ Hangman Game Stuff/Hangman Game Using PyGame/hangman.py
@@ -13,6 +13,53 @@ pink = (255,192,203)
 black = (0,0,0)
 green = (0,255,0)
 white = (255,255,255)
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+
+pygame.init()
+FONT = pygame.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
 
 class App:
     """docstring for App."""
@@ -371,27 +418,33 @@ class App:
 
 
     def playGame(self):
-        # https://stackoverflow.com/questions/46390231/how-to-create-a-text-input-box-with-pygame
         file_name = self.getWordFile()
         chosen_word, word_len = self.chooseWord(file_name)
 
-        strikes = 0
+        strikes = 6
         guesses = []
 
+        input_box = InputBox(600,300,10,32)
+        input_boxes = [input_box]
         user_pick = False
         while not user_pick:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self._running=False
+                for box in input_boxes:
+                    box.handle_event(event)
+                for box in input_boxes:
+                    box.update()
+                self.gameDisplay.fill((white))
+                for box in input_boxes:
+                    box.draw(self.gameDisplay)
 
-            self.gameDisplay.fill(white)
-            self.drawStickMan(6)
+            self.drawStickMan(strikes)
             self.updateInfo(10,strikes)
             self.updateLetterGuessed(guesses)
             pygame.Surface.blit(self.gameDisplay,self.hangmanSurface,(20,20))
             pygame.Surface.blit(self.gameDisplay,self.infoSurface,(320,20))
             pygame.Surface.blit(self.gameDisplay,self.lettersGuessedSurface,(480,20))
-
 
             pygame.display.update()
 
